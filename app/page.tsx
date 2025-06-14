5f7035f94346fb469f84da2b708a52dff8e88ac4
Kushagra Sahu <kushagra.sawho22@gmail.com>
2025-06-15 02:13:48 +0530

    Initial Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>
+        Welcome  to Neuralearn
+      </h1>
+      <Button >
+        Click Here to Start Learning
+      </Button>
+    </div>
+  )
+}
+
+export default Page
